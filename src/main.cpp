a8267fa26d8c435e82efd796102950d627b27203
James Kong <jkong@musiclifeltd.com>
2015-02-06 23:57:50 +0800

    added constrains

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,12 @@
 
 //========================================================================
 int main( ){
-	
-HWND handleWindow;  
+#ifdef TARGET_OS_WIN32
+HWND handleWindow;
 AllocConsole();  
 handleWindow = FindWindowA("ConsoleWindowClass", NULL);  
 ShowWindow(handleWindow, 0);  
-
+#endif
     ofAppGlutWindow window; // create a window
     // set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
     ofSetupOpenGL(&window, 1280, 720, OF_WINDOW);
